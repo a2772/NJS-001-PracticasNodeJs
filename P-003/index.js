279f96a205a0922918e72f8682941db22c03d587
Paris <paris.140815@gmail.com>
2022-09-10 23:53:56 -0500

    Errors management

--- P-003/index.js
+++ P-003/index.js
@@ -1,15 +1,32 @@
-let puerto = 3906
+const puerto = 3906
 const http = require('http')
 const server = http.createServer(onRequest)
+const fs = require('fs')
+
 server.listen(puerto, ()=>{
     console.log('Mi server está ejecutándose en localhost:'+puerto)
 })
 
 function onRequest(req, res){
     console.log('Se ha encontrado una nueva petición y esta es la respuesta')
-    //res.setHeader('Content-type', 'text/plain')
-    res.setHeader('Content-type', 'text/html')
-    res.write('<h1>Bienvenido a Node.js</h1>')
-    res.write('<p>Bienvenido a Node.js</p>')
-    res.end()
+    fs.readFile('index.html', (error,content)=>{
+        if(error){
+            switch(error.code){
+                case 'ENOENT':
+                    res.setStatus = 404
+                    console.log('No se ha encontrado el archivo html')
+                    break;
+                default:
+                    res.setStatus = 500
+                    console.log('Ha ocurrido un error en el servidor')
+                    break;
+            }
+            console.log(error)
+        }else{
+            res.setStatus = 202
+            res.setHeader('Content-type', 'text/html')
+            res.write(content)
+            res.end()
+        }
+    })
 }
